27c98db12ac9b7d97dd8c992164ff2a14855437f
Geriatric Assistance Robot <54467012+geriassist@users.noreply.github.com>
2020-05-06 13:15:36 -0700

    Update for debug
    Updated to support debug flag. When debug is enabled, output to serial port is allowed.

--- sdist1/sdist1.ino
+++ sdist1/sdist1.ino
@@ -15,6 +15,7 @@
 #define DISP_SPEED 30
 #define MAXMESS 5
 #define BANNERINT 5 //minutes
+const bool DBGOUT=false;
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 
@@ -32,7 +33,7 @@ int scrollPause = 50; // ms of pause after finished displaying message
 
 #define  BUF_SIZE  75  // Maximum of 75 characters
 char StartMsg [BUF_SIZE] = {"PDA V1.0"};
-char curMessage[MAXMESS][BUF_SIZE] = {{ "DON'T STAND SO CLOSE TO ME!!"},{"Social distancing is 6 feet."},{"Please get off my a$$."},{"Please stand back 6'" },{"I don't want the Rona! Please back off."}};  // used to hold current message
+char curMessage[MAXMESS][BUF_SIZE] = {{ "DON'T STAND SO CLOSE TO ME!!"}, {"Social distancing is 6 feet."}, {"Please get off my a$$."}, {"Please stand back 6'" }, {"I don't want the Rona! Please back off."}}; // used to hold current message
 char startupMessage[BUF_SIZE] = {"WWW.PERSONALDISTANCEALERT.COM"};
 char bannerMessage[BUF_SIZE] = {"WWW.PERSONALDISTANCEALERT.COM"};
 int myIndx = 0;
@@ -51,15 +52,17 @@ void setup()
   while (! Serial) {
     delay(1);
   }
-  
+
   Serial.println("Personal Distance Alert V 1.0");
-  // power 
+  // power
   P.begin();  // Start Parola
   // configure Parola
   P.setZoneEffect(0, true, PA_FLIP_LR);
   if (!lox.begin()) {
+    if (DBGOUT) {
     Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
+    }
+    while (1);
   }
 }
 
@@ -68,7 +71,7 @@ int SetNewMessage(int ndx) {
   if (retval > MAXMESS) {
     retval = 0;
   }
-  //Serial.printf("Setting new message %i\n", retval); 
+  //Serial.printf("Setting new message %i\n", retval);
   P.displayText(&curMessage[retval][0], PA_RIGHT, DISP_SPEED, scrollPause, PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
   //P.displayScroll(&curMessage[retval][0],PA_RIGHT, PA_SCROLL_RIGHT, DISP_SPEED);
   return retval;
@@ -77,61 +80,59 @@ int SetNewMessage(int ndx) {
 void ResetBannerTimer() {
   StartTime = millis();
   EndTime = StartTime + (BANNERINT * 60 * 1000);
+  if (DBGOUT) {
   Serial.printf("StartTime is %i EndTime is %i\n", StartTime, EndTime);
+  }
 }
 
 bool CheckBannerTimer() {
+  if (DBGOUT) {
   Serial.printf("EndTime is %i CurTime is %i\n", EndTime, millis());
+  }
   return millis() > EndTime;
 }
 
 void TriggerDisplay() {
   bool Done = false;
   myIndx = SetNewMessage(myIndx);
-  do {
-      if (P.displayAnimate()) {
-        Done = true;
-      }
-      delay(10);
-  } while (!Done);
+  Animator();
   needsRst = 1;
-  //P.displayClear();
-  //P.displayReset();
+}
+
+void Animator() {
+  bool Done = false;
+  do {
+    if (P.displayAnimate()) {
+      Done = true;
+    }
+    delay(10);
+  } while (!Done);
 }
 
 void DisplayBanner() {
   bool Done = false;
   P.displayText(bannerMessage, PA_RIGHT, DISP_SPEED, scrollPause, PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
-  do {
-      if (P.displayAnimate()) {
-        Done = true;
-      }
-      delay(10);
-  } while (!Done);
- 
+  Animator();
 }
 
 void loop() {
   VL53L0X_RangingMeasurementData_t measure;
   if (Startup) {
-      bool Done = false;
-      P.displayText(startupMessage, PA_RIGHT, DISP_SPEED, scrollPause, PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
-      do {
-        if (P.displayAnimate()) {
-          Done = true;
-        }
-        delay(10);
-      } while (!Done);
-      Startup = false;
+    bool Done = false;
+    P.displayText(startupMessage, PA_RIGHT, DISP_SPEED, scrollPause, PA_SCROLL_RIGHT, PA_SCROLL_RIGHT);
+    Animator();
+    Startup = false;
   }
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     int rm = measure.RangeMilliMeter;
-    
+
     if ((rm > MIN_DIST) && (rm < TRIG_DIST)) {
-      //Serial.printf("Trigger range is %i mm\n", rm);
+      if (DBGOUT) {
+      Serial.printf("Trigger range is %i mm\n", rm);
+      }
       TriggerDisplay();
       ResetBannerTimer();
 
@@ -140,7 +141,7 @@ void loop() {
         //Serial.printf("Range is %i mm\n", rm);
       }
     }
-  } 
+  }
   delay(500); //Wait for half a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(500);                       // wait for half a second
@@ -148,5 +149,5 @@ void loop() {
     DisplayBanner();
     ResetBannerTimer();
   }
-  
+
 }
